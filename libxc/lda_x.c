84d0396db2428bfea9d460f982251d974670d3a2
Carlo Andrea Rozzi <carloandrea.rozzi@nano.cnr.it>
2007-05-16 08:38:34 +0000

    this comment was confusing the pgi compiler

--- libxc/lda_x.c
+++ libxc/lda_x.c
@@ -85,7 +85,7 @@ void lda_x(lda_type *p, double *rho, double *ex, double *vx, double *fx)
   *ex /= dens;
 
   if(p->relativistic != 0){
-    // Relativistic corrections
+    /* Relativistic corrections */
     beta = pow(3.0*pow(M_PI, 2.0)*dens, 1.0/3.0)/M_C;
     phi = 1.0 - 3.0/2.0*pow(sqrt(1 + pow(beta, 2.0))/beta - asinh(beta)/pow(beta, 2.0), 2.0);
     DphiDdens = -2.0/dens/pow(beta, 2.0) * (-1.0 + asinh(beta)*(pow(beta, 2.0) + 2)/(beta*sqrt(1.0 + pow(beta, 2.0))) -

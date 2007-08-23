c09f9af01b50d76aab465e48a7148b3a47a48b74
Florian Lorenzen <florian.lorenzen@posteo.de>
2007-08-23 14:47:41 +0000

    Bugfix: backport of r3133 to the 2.1.x branch.

--- src/states.F90
+++ src/states.F90
@@ -1928,6 +1928,7 @@ contains
             dtmp(:), u, ierr, is_tmp = .false.)
         end do
       end do
+      deallocate(dtmp)
     end if
 
     if( (iand(outp%what, output_current).ne.0) .and. (st%d%wfs_type == M_CMPLX) ) then

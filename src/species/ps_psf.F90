bc175a64f7412c32d9203127781d83d5a1bcead2
Xavier Andrade <xavier@tddft.org>
2018-03-03 12:08:04 -0800

    A more general interface for the extrapolation used for pseudopotential parsing.

--- src/species/ps_psf.F90
+++ src/species/ps_psf.F90
@@ -310,7 +310,7 @@ contains
         vtot = psf_file%vps(ir, l) + ve(ir, 1) + dble((l-1)*l)/(g%rofi(ir)**2)
         hato(ir) = vtot*s(ir) + a2b4
       end do
-      hato(1) = linear_extrapolate(g%rofi(1), g%rofi(2), g%rofi(3), TOFLOAT(hato(2)), TOFLOAT(hato(3)))
+      hato(1) = first_point_extrapolate(g%rofi, hato)
 
       nnode = 1
       nprin = l
@@ -354,7 +354,7 @@ contains
               vtot = psf_file%vps(ir, l) + ve(ir, is) + dble((l-1)*l)/(g%rofi(ir)**2)
               hato(ir) = vtot*s(ir) + a2b4
             end do
-            hato(1) = linear_extrapolate(g%rofi(1), g%rofi(2), g%rofi(3), TOFLOAT(hato(2)), TOFLOAT(hato(3)))
+            hato(1) = first_point_extrapolate(g%rofi, hato)
 
             nnode = 1
             nprin = l
@@ -457,7 +457,7 @@ contains
         vtot = ps_grid%vlocal(ir) + ve(ir) + dble((l-1)*l)/(g%rofi(ir)**2)
         hato(ir) = vtot*s(ir) + a2b4
       end do
-      hato(1) = linear_extrapolate(g%rofi(1), g%rofi(2), g%rofi(3), TOFLOAT(hato(2)), TOFLOAT(hato(3)))
+      hato(1) = first_point_extrapolate(g%rofi, hato)
 
       do nnode = 1, 2
         nprin = l
